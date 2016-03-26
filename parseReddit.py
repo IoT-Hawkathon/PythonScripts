a1d32b8245f75e7e5afc5ba5cdcc03c4a3c6e34d
Sohit Pal <sohitpal@outlook.com>
2016-03-26 11:46:21 -0400

    Added comments and fixed a bug that stopped the script from printing the newly created file

--- parseReddit.py
+++ parseReddit.py
@@ -6,26 +6,29 @@ import urllib2
 import sys
 import os
 
+# Check for command line arguments, if none provided, default to todayilearned
 if len(sys.argv) == 1:
 	subreddit = "todayilearned"
 else:
 	subreddit = sys.argv[1]
 
-#TODO: change user agent string
-hdr = { 'User-Agent' : 'test user agent...' }
 
+# Set custom user agent, then request JSON file from Reddit
+hdr = { 'User-Agent' : 'test user agent...' }
 req = urllib2.Request('https://www.reddit.com/r/%s.json' % (subreddit,), headers=hdr)
 
+# Attempt to read the recieved content
 try:
 	html = urllib2.urlopen(req).read()
 except Exception:
 	print subreddit + " doesn't seem to be a real subreddit"
 	sys.exit(0)
 
+# Open the file
 fileName = subreddit + "_headlines.txt"
-
 outputFile = io.open(fileName, 'w', encoding='utf8')
 
+# Load the JSON file and parse its' contents
 data = json.loads(html)
 data2 = data['data']
 data3 = data2['children']
@@ -34,9 +37,9 @@ for d in data3:
 	#if "TIL" in toPrint:
 	outputFile.write(toPrint + '\n')
 
-#outputFile.close()
+# Close the file
+outputFile.close()
 
+# Print out the contents of the newly created file in the terminal
 command = "cat " + fileName 
-
-print "Printing Now..."
 os.system(command)
