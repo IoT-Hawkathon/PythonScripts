936d3e1e60abce3be47960673ca4c145d956c160
Sohit Pal <sohitpal@outlook.com>
2016-03-26 11:39:43 -0400

    Changed parseReddit script to allow parsing of any subreddit (given as a command line argument)

--- parseReddit.py
+++ parseReddit.py
@@ -3,20 +3,40 @@ import csv
 import io
 import json
 import urllib2
+import sys
+import os
+
+if len(sys.argv) == 1:
+	subreddit = "todayilearned"
+else:
+	subreddit = sys.argv[1]
 
 #TODO: change user agent string
 hdr = { 'User-Agent' : 'test user agent...' }
-req = urllib2.Request('https://www.reddit.com/r/todayilearned.json', headers=hdr)
-html = urllib2.urlopen(req).read()
 
-outputFile = io.open('TIL_OUTPUT.txt', 'w', encoding='utf8')
+req = urllib2.Request('https://www.reddit.com/r/%s.json' % (subreddit,), headers=hdr)
 
-#toParse = open("todayilearned.txt", 'rb')
+try:
+	html = urllib2.urlopen(req).read()
+except Exception:
+	print subreddit + " doesn't seem to be a real subreddit"
+	sys.exit(0)
+
+fileName = subreddit + "_headlines.txt"
+
+outputFile = io.open(fileName, 'w', encoding='utf8')
 
 data = json.loads(html)
 data2 = data['data']
 data3 = data2['children']
 for d in data3:
 	toPrint = d['data']['title']
-	if "TIL" in toPrint:
-		outputFile.write(toPrint + '\n')
+	#if "TIL" in toPrint:
+	outputFile.write(toPrint + '\n')
+
+#outputFile.close()
+
+command = "cat " + fileName 
+
+print "Printing Now..."
+os.system(command)
